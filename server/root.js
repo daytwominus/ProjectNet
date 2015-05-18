83bf6b2d13e23f8cf2603e1d1157497e80756d5f
daytwominus <panasiux@gmail.com>
2015-05-18 23:17:15 +0300

    1

--- server/root.js
+++ server/root.js
@@ -3,3 +3,31 @@ var app = express();
 var bodyParser = require('body-parser');
 var fs = require('fs');
 var node_static = require('node-static');
+
+var port = process.env.PORT || 3000;
+
+// ROUTES FOR OUR API
+// =============================================================================
+
+{
+    var router = express.Router();
+
+    // middleware to use for all requests
+    router.use(function(req, res, next) {
+        console.log('Something is happening.');
+        next(); // make sure we go to the next routes and don't stop here
+    });
+
+    router.get('/', function(req, res) {
+        res.json({ message: 'api works!' });
+    });
+
+
+    app.use('/api', router);
+}
+
+
+// START THE SERVER
+// =============================================================================
+app.listen(port);
+console.log('Magic happens on port ' + port);
