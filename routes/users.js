57bf961cdb1c9db6c1e530c481c01a295b0ce190
sergeypanasyuk <panasiux@gmail.com>
2015-05-20 16:45:37 +0300

    panasiux

--- routes/users.js
+++ routes/users.js
@@ -4,11 +4,21 @@ var users = require("../models/user");
 
 
 
+var users = require("../models/user");
+
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   //users.addUser({"id":"123xy"});
   //res.send('respond with a resource');
 
+});
+
+router.get('/:displayName', function(req, res, next) {
+  users.findUser({displayName : req.params.displayName}, function(err, user){
+    if (err)
+      res.send(err);
+    res.json(user);
+  });
 });
 
 
