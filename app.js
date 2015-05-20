f8106ed703d97cab926777846fae6123f6ef6b94
sergeypanasyuk <panasiux@gmail.com>
2015-05-20 15:34:11 +0300

    panasiux

--- app.js
+++ app.js
@@ -12,6 +12,7 @@ var users = require('./routes/users');
 var app = express();
 
 require('./helpers/logging')(app);
+require('./config/login')(app);
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -21,6 +22,8 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(passport.initialize());
 
 app.use('/', routes);
 app.use('/index', routes);
--- routes/login.js
+++ routes/login.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var router = express.Router();
+var passport = require('passport');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -8,10 +9,11 @@ router.get('/', function(req, res, next) {
 
 router.post('/',function(){
         console.log("posted");
+        passport.authenticate('local', { successRedirect: '/',
+            failureRedirect: '/login',
+            failureFlash: true })
     }
-    //passport.authenticate('local', { successRedirect: '/',
-    //    failureRedirect: '/login',
-    //    failureFlash: true })
+
 );
 
 module.exports = router;
