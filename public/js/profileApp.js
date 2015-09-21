5c8a62af16e27b5860c35d9662c8710276393f0e
daytwominus <panasiux@gmail.com>
2015-09-21 22:41:53 +0300

    1

--- public/js/profileApp.js
+++ public/js/profileApp.js
@@ -1,6 +1,6 @@
-var profileApp = angular.module('profileApp', []);
+var profileApp = angular.module('profileApp', ['angularFileUpload']);
 
-profileApp.controller('profileController', function ($scope, profileFactory) {
+profileApp.controller('profileController', function ($scope, profileFactory, FileUploader) {
 
     $scope.updateUser = function(){
         profileFactory.updateProfile($scope.user)
@@ -20,6 +20,58 @@ profileApp.controller('profileController', function ($scope, profileFactory) {
         .error(function(error){
             console.log(error);
         });
+
+    var uploader = $scope.uploader = new FileUploader({
+        url: 'rest/uploadAvatar',
+        autoUpload:true
+    });
+
+    // FILTERS
+
+    uploader.filters.push({
+        name: 'customFilter',
+        fn: function(item /*{jpg|jpeg|png}*/, options) {
+            return this.queue.length < 10;
+        }
+    });
+
+    // CALLBACKS
+
+    uploader.onWhenAddingFileFailed = function(item /*{File|FileLikeObject}*/, filter, options) {
+        console.info('onWhenAddingFileFailed', item, filter, options);
+    };
+    uploader.onAfterAddingFile = function(fileItem) {
+        console.info('onAfterAddingFile', fileItem);
+    };
+    uploader.onAfterAddingAll = function(addedFileItems) {
+        console.info('onAfterAddingAll', addedFileItems);
+    };
+    uploader.onBeforeUploadItem = function(item) {
+        console.info('onBeforeUploadItem', item);
+    };
+    uploader.onProgressItem = function(fileItem, progress) {
+        console.info('onProgressItem', fileItem, progress);
+    };
+    uploader.onProgressAll = function(progress) {
+        console.info('onProgressAll', progress);
+    };
+    uploader.onSuccessItem = function(fileItem, response, status, headers) {
+        console.info('onSuccessItem', fileItem, response, status, headers);
+    };
+    uploader.onErrorItem = function(fileItem, response, status, headers) {
+        console.info('onErrorItem', fileItem, response, status, headers);
+    };
+    uploader.onCancelItem = function(fileItem, response, status, headers) {
+        console.info('onCancelItem', fileItem, response, status, headers);
+    };
+    uploader.onCompleteItem = function(fileItem, response, status, headers) {
+        console.info('onCompleteItem', fileItem, response, status, headers);
+    };
+    uploader.onCompleteAll = function() {
+        console.info('onCompleteAll');
+    };
+
+    console.info('uploader', uploader);
 });
 
 profileApp.factory('profileFactory', function($http){
--- routes/rest.js
+++ routes/rest.js
@@ -1,5 +1,9 @@
 var express = require('express');
 var router = express.Router();
+var multer  = require('multer');
+var upload = multer({ dest: 'upload/'});
+var fs = require('fs');
+
 
 router.get('/profile', function(req, res, next) {
 
@@ -12,5 +16,43 @@ router.post('/profile', function(req, res, next) {
     console.log(req.body);
     res.send(200);
 });
+
+/*router.post('/uploadAvatar', upload.single('avatar'), function(req, res, next) {
+    console.log(req.files);
+
+    //handler(req, res, next);
+});
+*/
+/*var type = upload.single('avatar');
+
+router.post('/uploadAvatar', type, function (req,res) {
+
+
+    var tmp_path = req.file.path;
+
+
+    var target_path = 'uploads/' + req.file.originalname;
+
+
+    var src = fs.createReadStream(tmp_path);
+    var dest = fs.createWriteStream(target_path);
+    src.pipe(dest);
+    src.on('end', function() { res.render('complete'); });
+    src.on('error', function(err) { res.render('error'); });
+
+});
+*/
+
+var cpUpload = upload.fields([{ name: 'file', maxCount: 1 }, { name: 'gallery', maxCount: 8 }])
+router.post('/uploadAvatar', cpUpload, function (req, res, next) {
+    console.log('!!!', req.files);
+    // req.files is an object (String -> Array) where fieldname is the key, and the value is array of files
+    //
+    // e.g.
+    //  req.files['avatar'][0] -> File
+    //  req.files['gallery'] -> Array
+    //
+    // req.body will contain the text fields, if there were any
+})
 
 module.exports = router;
