ddb87af64f7dce5dd429a75d09529a71b71899de
sergeypanasyuk <panasiux@gmail.com>
2015-10-07 18:41:20 +0300

    panasiux

--- routes/home.js
+++ routes/home.js
@@ -4,6 +4,9 @@ var router = express.Router();
 /* GET home page. */
 router.get('/', function(req, res, next) {
     console.log("getting home page");
+    res.header("Cache-Control", "no-cache, no-store, must-revalidate");
+    res.header("Pragma", "no-cache");
+    res.header("Expires", 0);
     res.render('home');
 });
 
