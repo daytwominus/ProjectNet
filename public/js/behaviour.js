d606ce2834b7172def7ac35e9f52a0ba33159ad5
sphere <panasiux@gmail.com>
2015-11-09 23:58:42 +0300

    vfd

--- public/js/behaviour.js
+++ public/js/behaviour.js
@@ -1,6 +1,9 @@
 window.onscroll=function(){
     if ((window.innerHeight + window.scrollY) >= document.body.offsetHeight) {
-        //document.getElementById("footerItem").style.height="180px";
-        console.log(document.getElementById("footerItem").style.height);
+        document.getElementById("footerItem").style.bottom="0px";
+        console.log(document.getElementById("footerItem").style.bottom);
+    }
+    else{
+        document.getElementById("footerItem").style.bottom="-40px";
     }
 };
