0ad73760c2665f374191748c64f98b8840903f97
sergeypanasyuk <panasiux@gmail.com>
2015-11-20 17:57:05 +0300

    panasiux

--- routes/login.js
+++ routes/login.js
@@ -13,6 +13,7 @@ router.get('/', function(req, res, next) {
 router.post('/',
     passport.authenticate('local', { failureRedirect: '/login', failureFlash: true }),
     function(req, res) {
+        console.log('login occured ', req.user);
         res.redirect('/home');
     });
 
