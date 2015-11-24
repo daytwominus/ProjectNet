cb027a6c972ef50847bb728a8f22b5a23acc1534
sphere <panasiux@gmail.com>
2015-11-24 22:07:37 +0300

    vfd

--- public/js/behaviour.js
+++ public/js/behaviour.js
@@ -1,9 +1,25 @@
-window.onscroll=function(){
-    if ((window.innerHeight + window.scrollY) >= document.body.offsetHeight) {
-        document.getElementById("footerItem").style.bottom="0px";
-        console.log(document.getElementById("footerItem").style.bottom);
+$('.footerItem').hover(
+    function (){
+        $('.footerItem').animate({
+            bottom: '0px',
+            background: '#ccc'
+        }, 100);
+    },
+    function (){
+        $('.footerItem').animate({
+            bottom: '-40px',
+            background: '#ccc'
+        }, 100);
     }
-    else{
-        document.getElementById("footerItem").style.bottom="-40px";
-    }
-};
+);
+
+
+//window.onscroll=function(){
+//    if ((window.innerHeight + window.scrollY) >= document.body.offsetHeight) {
+//        document.getElementById("footerItem").style.bottom="0px";
+//        console.log(document.getElementById("footerItem").style.bottom);
+//    }
+//    else{
+//        document.getElementById("footerItem").style.bottom="-40px";
+//    }
+//};
